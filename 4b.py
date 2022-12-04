b00aebd3afdde3d6571b4eb78022c8a751cab7d9
kennonlee <kennon_lee@yahoo.com>
2022-12-04 21:47:23 +0100

    Add files via upload
    cleanup

--- 4b.py
+++ 4b.py
@@ -7,21 +7,8 @@ def intersect (x1, x2, y1, y2):
         return False
     return True
 
-def superset (x1, x2, y1, y2):
-    if x1 == y1 and x2 == y2:
-        print("not superset: " + str(x1) + "-" + str(x2) + "," + str(y1) + "-" + str(y2))
-        return False
-    if x1 <= y1 and x2 >= y2:
-#        print("superset: " + str(x1) + "-" + str(x2) + "," + str(y1) + "-" + str(y2))
-        return True
-    if y1 <= x1 and y2 >= x2:
-#        print("superset: " + str(x1) + "-" + str(x2) + "," + str(y1) + "-" + str(y2))
-        return True
-    return False
-
 count = 0
 for line in sys.stdin:
-#    print(line)
     a, b = line.split(',')
     a1, a2 = [int(i) for i in a.split('-')]
     b1, b2 = [int(i) for i in b.split('-')]
